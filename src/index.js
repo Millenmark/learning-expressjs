6b5c27f51037e33b2d367211634aa77fb6f0720f
Millenmark <millenmark.iv@gmail.com>
2023-05-05 19:41:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const groceryRoute = require("./routes/groceryRoute")
+const marketRoute = require("./routes/marketRoute")
 
 const app = express()
 const PORT = 5000
@@ -9,6 +10,7 @@ app.use(express.json())
 
 //use the groceryRoute from routes NOTE: This should be invoked before all the middleware
 app.use("/api/groceries", groceryRoute)
+app.use("/api/markets", marketRoute)
 
 
 app.listen(PORT, () => console.log(`Running express server on port: ${PORT}`))
