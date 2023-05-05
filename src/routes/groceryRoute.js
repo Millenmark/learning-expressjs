7ccadd1858761efe7674e3f4dbdffdcab0ee9abf
Millenmark <millenmark.iv@gmail.com>
2023-05-05 19:25:21 +0800

    Update groceryRoute.js

--- src/routes/groceryRoute.js
+++ src/routes/groceryRoute.js
@@ -13,19 +13,19 @@ const groceryList = [
   }
 ]
 
-router.get("/groceries", (req, res) => {
+router.get("/", (req, res) => {
   res.send(groceryList)
 })
 
 //Route Parameters /:item NOTE: A route parameter always prefix with a colon
-router.get("/groceries/:item", (req, res) => {
+router.get("/:item", (req, res) => {
   // console.log(req.params.item)
   const { item } = req.params
   const groceryItem = groceryList.find(g => g.item === item)
   res.send(groceryItem)
 })
 
-router.post("/groceries", (req, res) => {
+router.post("/", (req, res) => {
   console.log(req.body)
   groceryList.push(req.body)
   res.sendStatus(201)
