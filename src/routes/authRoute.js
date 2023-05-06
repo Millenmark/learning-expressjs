8c587a899ef971f0bfc8a9a36d2bddc5568f0f2d
Millenmark <millenmark.iv@gmail.com>
2023-05-06 19:53:49 +0800

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -6,27 +6,28 @@ const router = Router()
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body
-  if (username && password) {
-    if (req.session.user) {
-      res.send(req.session.user)
-    } else {
-      req.session.user = {
-        username
-      }
-      res.send(req.session)
-    }
-  } else res.sendStatus(401)
+  // if (username && password) {
+  //   if (req.session.user) {
+  //     res.send(req.session.user)
+  //   } else {
+  //     req.session.user = {
+  //       username
+  //     }
+  //     res.send(req.session)
+  //   }
+  // } else res.sendStatus(401)
+
 })
 
 router.post("/register", async (req, res) => {
-  const { username, email } = req.body
-  const userDB = await userModel.findOne({ $or: [{username}, {email}]})
+  const { email } = req.body
+  const userDB = await userModel.findOne({email})
   if (userDB) {
     res.status(400).send({message: "User already exists!"})
   } else {
     const password = hashPassword(req.body.password)
     console.log(password)
-    const newUser = await userModel.create({username, password, email})
+    await userModel.create({username, password, email})
     res.sendStatus(201)
   }
 })
