d572e250cf9359c68107ce4a667d6b8de7387301
Millenmark <millenmark.iv@gmail.com>
2023-05-06 19:04:56 +0800

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,4 +1,5 @@
 const { Router } = require("express")
+const userModel = require("../database/schemas/userSchema")
 
 const router = Router()
 
@@ -14,6 +15,17 @@ router.post("/login", (req, res) => {
       res.send(req.session)
     }
   } else res.sendStatus(401)
+})
+
+router.post("/register", async (req, res) => {
+  const { username, password, email } = req.body
+  const userDB = await userModel.findOne({ $or: [{username}, {email}]})
+  if (userDB) {
+    res.status(400).send({message: "User already exists!"})
+  } else {
+    const newUser = await userModel.create({username, password, email})
+    res.sendStatus(201)
+  }
 })
 
 module.exports = router
