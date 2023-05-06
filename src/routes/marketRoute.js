159e34892d4db37f6f4f980f3b68e12a28c1d683
Millenmark <millenmark.iv@gmail.com>
2023-05-06 10:29:41 +0800

    Update marketRoute.js

--- src/routes/marketRoute.js
+++ src/routes/marketRoute.js
@@ -6,19 +6,33 @@ const superMarkets = [
   {
     _id: "1",
     store: "Whole Foods",
+    miles: 0.6
   },
   {
     _id: "2",
-    store: "FoodTech"
+    store: "FoodTech",
+    miles: 2.5
   },
   {
     _id: "3",
-    store: "Hamburger Milktea"
+    store: "Hamburger Milktea",
+    miles: 4.8
+  },
+  {
+    _id: "4",
+    store: "Hamburger Milktea",
+    miles: 8.8
   }
 ]
 
 router.get("/", (req, res) => {
-  res.send(superMarkets)
+  // console.log(req.query)
+  const { miles } = req.query //the value of miles is a string so you need to convert it into number
+  const parsedMiles = parseInt(miles) //No need to use if(miles) because we parsed it into a number and it will become undefined if the miles is not existing or not a truthy value
+  if (!isNaN(parsedMiles)) {
+    const filteredStores = superMarkets.filter(s => s.miles <= parsedMiles)
+    res.send(filteredStores)
+  } else res.send(superMarkets)
 })
 
 router.get("/find/:id", (req, res) => {
