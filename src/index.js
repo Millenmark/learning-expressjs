c40a796d29221b7a4fc218621730b18ddb3df24f
Millenmark <millenmark.iv@gmail.com>
2023-05-06 15:55:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const session = require("express-session")
 const groceryRoute = require("./routes/groceryRoute")
 const marketRoute = require("./routes/marketRoute")
 
@@ -9,6 +10,11 @@ const PORT = 5000
 //middleware
 app.use(express.json())
 app.use(cookieParser())
+app.use(session({
+  secret: "KomarShawadika",
+  resave: false,
+  saveUninitialized: false,
+}))
 
 //use the groceryRoute from routes NOTE: This should be invoked before all the middleware
 app.use("/api/groceries", groceryRoute)
